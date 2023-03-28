89c2600047ce8276822142eecc88310002cfb4cf
mGunawardhana <mrgunawardhana27368@gmail.com>
2023-03-28 13:36:03 +0530

    learning click events with react

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -1,5 +1,6 @@
 import { type } from "os";
 import { Component } from "react";
+import Counter from "../Counter";
 import Customer from "../Customer";
 
 
@@ -11,6 +12,7 @@ export default class Content extends Component {
       <div className="flex items-center justify-center h-screen">
         <Customer {...{ id: "C001", name: "Maneesha", address: "Galle", salary: 12000.00 }} />
         <Customer {...{ id: "C002", name: "Dinithi", address: "Galle", salary: 12000.00 }} />
+        <Counter {...{initValue:10}} />
       </div>
     );
   }
--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -1,17 +1,43 @@
 import React, { Component } from 'react'
 
-type CounterProps = {
-    initValue: number;
-}
+type CounterProps = {initValue: number;};
 
-type CounterState = {
-    count: number;
-}
+type CounterState = { count: number; };
 
-export default class Counter extends Component <CounterProps,CounterState> {
+
+export default class Counter extends Component<CounterProps,CounterState> {
+    constructor(props:CounterProps) {
+        super(props);
+        this.state = {
+            count: this.props.initValue,
+        };
+    }
+
+    increaseValue = () => {
+        this.setState({count:this.state.count+1});
+    };
+
+    decreaseValue = () => {
+        this.setState({ count: this.state.count - 1 })
+    };
+
   render() {
     return (
-      <div>Counter</div>
+     <div className='h-80 border
+       bg-slate-800 text-white px-32 flex justify-center items-center
+        flex-col gap-6 rounded-lg shadow-lg'>
+            <h1>{this.state.count}</h1>
+            <div className='flex gap-6 '>
+                <button
+                    className='text-4xl border-2 w-16 h-10 rounded-lg'
+                    onClick={this.decreaseValue }
+                >-</button>
+                <button className='text-4xl border-2 w-16 h-10 rounded-lg'
+                    onClick={ this.increaseValue}
+                >+</button>
+            </div>
+        </div>
     )
+      //01:01:09
   }
 }
